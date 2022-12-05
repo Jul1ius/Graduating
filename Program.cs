e270a8f7b56aba278106975a07446908aba180b9
its julii <nanji828@gmail.com>
2022-12-05 21:24:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Graduation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int grade = 1;
+            int excludedGrade = 0;
+            double totalGrade = 0;
+
+            while (grade <= 12)
+            {
+                double secGrade = double.Parse(Console.ReadLine());
+
+                if (secGrade < 4)
+                {
+                    excludedGrade++;
+                }
+                if (excludedGrade == 2)
+                {
+                    Console.WriteLine($"{name} has been excluded at {grade - 1} grade");
+                    break;
+                }
+                totalGrade += secGrade;
+                grade++;
+            }
+            double average = totalGrade / 12;
+            if (excludedGrade < 2)
+            {
+                Console.WriteLine($"{name} graduated. Average grade: {average:f2}");
+            }
+        }
+    }
+}
+
